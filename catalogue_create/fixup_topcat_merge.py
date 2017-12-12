28e5ea4a70d3f2ae44581e771bef269452040feb
Martin Hardcastle <mjh@extragalactic.info>
2017-12-12 21:10:20 +0000

    Add stilts command line

--- catalogue_create/fixup_topcat_merge.py
+++ catalogue_create/fixup_topcat_merge.py
@@ -1,7 +1,10 @@
 from astropy.table import Table
 
-# run a topcat or stilts merge with max sep 10 arcsec on ID_ra and ra, ID_dec
-# and dec. Save the result as merge.fits. Then
+# run the following
+
+# /soft/topcat/stilts tskymatch2 ra1=ID_ra dec1=ID_dec ra2=ra dec2=dec error=10 out=merge.fits find=best1 join=all1 in1=LOFAR_HBA_T1_DR1_merge_ID_v0.6.fits in2=/data/lofar/mjh/hetdex_ps1_allwise_photoz_v0.2.fits
+
+# and then run this to clean up afterwards.
 
 t=Table.read('merge.fits')
 
@@ -13,4 +16,4 @@ t['ID_dec']=t['dec_2']
 
 t.remove_columns(['ra_2','dec_2','GroupID','GroupSize','Separation','raMean','decMean','class','id','z_good'])
 t.sort('RA')
-t.write('LOFAR_HBA_T1_DR1_merge_ID_optical_v0.2.fits',overwrite=True)
+t.write('LOFAR_HBA_T1_DR1_merge_ID_optical_v0.3.fits',overwrite=True)
