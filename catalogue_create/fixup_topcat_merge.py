677051fd699e634648429d2b7c9afb8f1a1b988f
Martin Hardcastle <mjh@extragalactic.info>
2017-12-17 09:58:14 +0000

    Count LGZ associations even for unchanged sources

--- catalogue_create/fixup_topcat_merge.py
+++ catalogue_create/fixup_topcat_merge.py
@@ -2,7 +2,7 @@ from astropy.table import Table
 
 # run the following
 
-# /soft/topcat/stilts tskymatch2 ra1=ID_ra dec1=ID_dec ra2=ra dec2=dec error=10 out=merge.fits find=best1 join=all1 in1=LOFAR_HBA_T1_DR1_merge_ID_v0.6.fits in2=/data/lofar/mjh/hetdex_ps1_allwise_photoz_v0.2.fits
+# /soft/topcat/stilts tskymatch2 ra1=ID_ra dec1=ID_dec ra2=ra dec2=dec error=10 out=merge.fits find=best1 join=all1 in1=LOFAR_HBA_T1_DR1_merge_ID_v0.7.fits in2=/data/lofar/mjh/hetdex_ps1_allwise_photoz_v0.2.fits
 
 # and then run this to clean up afterwards.
 
@@ -14,6 +14,6 @@ t['DEC_1'].name='DEC'
 t['ID_ra']=t['ra_2']
 t['ID_dec']=t['dec_2']
 
-t.remove_columns(['ra_2','dec_2','GroupID','GroupSize','Separation','raMean','decMean','class','id','z_good'])
+t.remove_columns(['ra_2','dec_2','GroupID','GroupSize','Separation','raMean','decMean','class','id','z_good','Number_Masked','Number_Pointings','Masked_Fraction'])
 t.sort('RA')
-t.write('LOFAR_HBA_T1_DR1_merge_ID_optical_v0.3.fits',overwrite=True)
+t.write('LOFAR_HBA_T1_DR1_merge_ID_optical_v0.5.fits',overwrite=True)
