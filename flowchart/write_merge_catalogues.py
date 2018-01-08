c7368f729057a5a9c37d4aa95d1f5f7d6f2c2002
Wendy L. Williams <wndywllms@gmail.com>
2018-01-08 14:32:11 +0000

    treat 2MASS sources more like LGZ -- quality flags and sizes for merged sources

--- flowchart/write_merge_catalogues.py
+++ flowchart/write_merge_catalogues.py
@@ -2,6 +2,9 @@ import os
 from astropy.table import Table, Column, join, vstack
 from astropy.coordinates import SkyCoord
 import numpy as np
+
+from catalogue_create.process_lgz import Make_Shape
+#from catalogue_create import process_lgz
 '''
 ID_flag
 1 - ML
@@ -193,6 +196,9 @@ if __name__=='__main__':
     lofarcat_sorted['ID_ra'][sel2mass] = lofarcat_sorted['2MASX_ra'][sel2mass]
     lofarcat_sorted['ID_dec'][sel2mass] = lofarcat_sorted['2MASX_dec'][sel2mass]
     
+    #lofarcat_sorted['LGZ_Assoc'][sel2mass] = 1
+    #lofarcat_sorted['LGZ_Assoc_Qual'][sel2mass] = 1.
+    #lofarcat_sorted['LGZ_ID_Qual'][sel2mass] = 1.
     
     # some sources come from a tag 'match to bright galaxy' - not necesarily 2MASX - look in SDSS for these:
     sel2masssdss = (lofarcat_sorted['ID_flag']==2) & (lofarcat_sorted['ID_name']=='2MASX J')
@@ -239,7 +245,13 @@ if __name__=='__main__':
     lofarcat_sorted['ID_dec'][sel2masssdss] = sdss_dec
     
     lofarcat_sorted.add_column(Column(np.nan*np.zeros(len(lofarcat_sorted),dtype=float),'LGZ_Size'))
+    lofarcat_sorted.add_column(Column(np.nan*np.zeros(len(lofarcat_sorted),dtype=float),'LGZ_Width'))
+    lofarcat_sorted.add_column(Column(np.nan*np.zeros(len(lofarcat_sorted),dtype=float),'LGZ_PA'))
     lofarcat_sorted.add_column(Column(np.nan*np.zeros(len(lofarcat_sorted),dtype=float),'LGZ_Assoc'))
+    
+    
+    lofarcat_sorted.add_column(Column(np.nan*np.zeros(len(lofarcat_sorted),dtype=float),'LGZ_Assoc_Qual'))
+    lofarcat_sorted.add_column(Column(np.nan*np.zeros(len(lofarcat_sorted),dtype=float),'LGZ_ID_Qual'))
 
     ok_matches =  (names != '2MASXJ')
     
@@ -248,7 +260,7 @@ if __name__=='__main__':
     lofarcat_add_2mass_mult = lofarcat_sorted[1:1]
     nmerge = np.sum(ucounts>1)
     nn = 0
-    for n in unames[ucounts>1]:
+    for n in unames: #[ucounts>1]:
         
         i = np.where(lofarcat_sorted['ID_name'] == n)[0]
         nn += len(i)
@@ -257,6 +269,7 @@ if __name__=='__main__':
         
         complist = lofarcat_sorted[i]
         assoc_2mass = lofarcat_sorted[i[0]]
+        
         
         assoc_2mass['RA']=np.average(complist['RA'], weights=complist['Total_flux'])
         assoc_2mass['DEC']=np.average(complist['DEC'], weights=complist['Total_flux'])
@@ -275,17 +288,29 @@ if __name__=='__main__':
         assoc_2mass['E_Peak_flux']=complist[maxpk]['E_Peak_flux']
         # merging multiple S/M will be M
         assoc_2mass['S_Code'] = 'M'
-        for t in ['Maj', 'Min', 'PA','E_Maj', 'E_Min', 'E_PA']:
+        for t in ['Maj', 'Min', 'PA']:
             assoc_2mass[t] = np.nan
+            assoc_2mass['E_'+t] = np.nan
+            assoc_2mass['DC_'+t] = np.nan
+            assoc_2mass['E_DC_'+t] = np.nan
+            
         assoc_2mass['Isl_id'] = -99
         
-        c =SkyCoord(complist['RA'], complist['DEC'], unit='deg')
-                
-
-        # size is max of Maj or sep between centres
-        assoc_2mass['LGZ_Size']=np.max([np.max(complist['Maj']) , np.max([ci.separation(c).max().to('arcsec').value for ci in c])])
+        #c =SkyCoord(complist['RA'], complist['DEC'], unit='deg')
         # TBD 'Mosiac_ID'
+        
+        # size is taken from convex hull of components - as in LGZ process
+        cshape = Make_Shape(complist)
+        assoc_2mass['LGZ_Size'] = cshape.length()
+        assoc_2mass['LGZ_Width'] = cshape.width()
+        assoc_2mass['LGZ_PA'] = cshape.pa()
+        
+        
         assoc_2mass['LGZ_Assoc'] = len(complist)
+        
+        # give them quality flags like LGZ
+        assoc_2mass['LGZ_Assoc_Qual'] = 1.
+        assoc_2mass['LGZ_ID_Qual'] = 1.
                 
         # to save the new names
         for c in complist:
