38f3a06067df6d3121b7e7ba8ca50d5059dfacdf
Wendy L. Williams <wndywllms@gmail.com>
2018-01-24 14:45:05 +0000

    update flag edge sources
    fix bug - ds9 region ellipse pa definition is out by -90 deg
    fix bug - include all sources
    further flag sources overlapping with already flagged edge sources

--- flowchart/flag_edge_sources.py
+++ flowchart/flag_edge_sources.py
@@ -10,6 +10,8 @@ matplotlib.use('Agg')
 from astropy.table import Table,vstack,Column
 from astropy.io import fits
 from astropy.wcs import WCS
+import astropy.coordinates as ac
+import astropy.units as u
 import numpy as np
 import sys
 import os
@@ -53,7 +55,7 @@ def check_flagged(mosaicname,ra,dec,size):
     return flagged
 
 
-def check_flagged_region(mosaicname,ra,dec,maj,min,pa):
+def check_flagged_region(mosaicname,ra,dec,maj,min,pa,plot=False):
     
     mosaicfile = get_mosaic_name(mosaicname)
     
@@ -64,12 +66,21 @@ def check_flagged_region(mosaicname,ra,dec,maj,min,pa):
         print mosaicname,ra,dec,maj
         return True
         
-    region = pyregion.parse('fk5;ellipse({ra},{dec},{a},{b},{pa})'.format(ra=ra,dec=dec,a=maj,b=min,pa=pa))
+    region = pyregion.parse('fk5;ellipse({ra},{dec},{a},{b},{pa})'.format(ra=ra,dec=dec,a=maj,b=min,pa=pa-90))
     
     data = lhdu[0].data
     region_mask = region.get_mask(lhdu[0])
     
     flagged = np.any(np.isnan(data[region_mask]))
+    
+    if plot:
+        plt.figure()
+        plt.subplot(131)
+        plt.imshow(data)
+        plt.subplot(132)
+        plt.imshow(region_mask)
+        plt.subplot(133)
+        plt.imshow(data*region_mask)
     
     return flagged
 
@@ -92,7 +103,7 @@ if __name__ == '__main__':
     Ns = ncpu
     N = ncpu*1200
     nanflags = np.zeros(len(lofarcat_all),dtype=bool)
-    for i in range(Ns):
+    for i in range(Ns+1):
         i2 = np.min((len(lofarcat_all),(i+1)*N)) 
         print 'chunk {}: {}-{} '.format(i,i*N, i2)
         starttime = time.time()
@@ -101,7 +112,7 @@ if __name__ == '__main__':
         nanflags[i*N:i2] = np.array(results)
         endtime = time.time()
     
-        print 'Took {} seconds for {} sources' .format(endtime-starttime, len(lofarcat))
+        print 'Took {:.2f} seconds for {} sources' .format(endtime-starttime, len(lofarcat))
     
     # take a closer look at the ones that have been flagged as having a nanpixel within Maj
     # apply region mask and check for nan pixels inside that only
@@ -110,19 +121,50 @@ if __name__ == '__main__':
     for ti in np.where(nanflags)[0]:
         tt = lofarcat_all[ti]
         newflag = check_flagged_region(tt['Mosaic_ID'],tt['RA'],tt['DEC'],tt['Maj']/3600.,tt['Min']/3600.,tt['PA'])
-        print ti, newflag
+        print ti, tt['Source_Name'], newflag
         nanflags2[ti] = newflag
+    
+    # finally check if any flagged sources overlap with other unflagged sources - these are probably bad too, unless it is a huge source that is flagged
+    # could be more sophisticated and check for actual overlap, not merely proximity based on Maj... but this seems to work.
+    nanflags3 = nanflags2.copy()
+    
+    
+    catc = ac.SkyCoord(lofarcat_all['RA'],lofarcat_all['DEC'],unit='deg')
+    for ti in np.where(nanflags2)[0]:
+        tt = lofarcat_all[ti]
+        if tt['Maj'] < 45:
+            catt = ac.SkyCoord(tt['RA'],tt['DEC'],unit='deg')
+            sep = catt.separation(catc)
+            nearby = np.where((sep<tt['Maj']*u.arcsec) & (sep>0*u.arcsec))[0]
+            if len(nearby) >=  1:
+                for ni in nearby:
+                    if not nanflags2[ni]:
+                        print tt['Source_Name'], tt['Maj'], '- also flagging ', lofarcat_all['Source_Name'][ni]
+                    
+                        nanflags3[ni] = True
+            #lofarcat_all[nearby]
+
     
     
     lofarcat_all.add_column(Column(nanflags, 'Edge_flag1'))
     lofarcat_all.add_column(Column(nanflags2, 'Edge_flag2'))
+    lofarcat_all.add_column(Column(nanflags3, 'Edge_flag3'))
     
     lofarcat_all.write(lofarcat_flagged_file,overwrite=True)
+    
+    print '{} edge1 flags'.format(np.sum(lofarcat_all['Edge_flag1']))
+    print '{} edge2 flags'.format(np.sum(lofarcat_all['Edge_flag2']))
+    print '{} edge3 flags'.format(np.sum(lofarcat_all['Edge_flag3']))
     
     
     # plot where the edges are
     plt.figure()
-    plt.scatter(lofarcat_all['RA'],lofarcat_all['DEC'],c=1.*nanflags2,edgecolor='none')
+    plt.scatter(lofarcat_all['RA'],lofarcat_all['DEC'],edgecolor='none')
+    plt.scatter(lofarcat_all['RA'][nanflags],lofarcat_all['DEC'][nanflags],edgecolor='none')
+    plt.scatter(lofarcat_all['RA'],lofarcat_all['DEC'],edgecolor='none')
+    plt.scatter(lofarcat_all['RA'][nanflags2],lofarcat_all['DEC'][nanflags2],edgecolor='none')
+    plt.scatter(lofarcat_all['RA'],lofarcat_all['DEC'],edgecolor='none')
+    plt.scatter(lofarcat_all['RA'][nanflags3],lofarcat_all['DEC'][nanflags3],edgecolor='none')
     #plt.scatter(lofarcat['RA'][nanflags],lofarcat['DEC'][nanflags],c='y',edgecolor='none')
     plt.savefig('edgeflags.png')
     
@@ -134,12 +176,23 @@ if __name__ == '__main__':
                     'ILTJ142342.77+524831.0',
                     'ILTJ141236.02+512257.8',
                     'ILTJ124236.50+562844.6',
-                    'ILTJ114646.72+562353.2']
+                    'ILTJ114646.72+562353.2',
+                    'ILTJ122510.05+460000.4',
+                    'ILTJ122510.79+455955.5',
+                    'ILTJ125725.79+561852.2',
+                    'ILTJ125726.89+561842.4']
     
     for n in checklist: 
-        print n, n in lofarcat_all[nanflags2]['Source_Name']
+        print n, n in lofarcat_all[nanflags]['Source_Name'] , n in lofarcat_all[nanflags2]['Source_Name'] , n in lofarcat_all[nanflags3]['Source_Name']
         
     ## hmm, two not flagged - one is in a messy part of the sky, the other is near the edge but doesn't quite make the cut
 
 
+    ## testing
     
+    nn = (lofarcat_all['Source_Name'] == 'ILTJ122510.05+460000.4') | (lofarcat_all['Source_Name'] == 'ILTJ122510.79+455955.5') 
+    for ti in np.where(nn)[0]:
+        tt = lofarcat_all[ti]
+        flag = check_flagged(tt['Mosaic_ID'],tt['RA'],tt['DEC'],tt['Maj']/3600.)
+        newflag =  check_flagged_region(tt['Mosaic_ID'],tt['RA'],tt['DEC'],tt['Maj']/3600.,tt['Min']/3600.,tt['PA'], plot=True)
+        print ti, tt['Source_Name'], flag, newflag
