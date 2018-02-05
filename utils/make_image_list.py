2e6365a91c0bac0ce48607be92a8ecce6c310b34
Martin Hardcastle <mjh@extragalactic.info>
2018-02-05 08:32:20 +0000

    LGZ v2 changes

--- utils/make_image_list.py
+++ utils/make_image_list.py
@@ -10,6 +10,8 @@ import glob
 import numpy as np
 import warnings
 from astropy.utils.exceptions import AstropyWarning
+import pickle
+from separation import separation
 
 class MapsList(object):
     def __init__(self,globstring,hdu_no=0):
@@ -20,6 +22,8 @@ class MapsList(object):
         files=[]
         ras=[]
         decs=[]
+        sizes=[]
+        wcs=[]
         for d in g:
             hdu=fits.open(d)
             xsize=hdu[hdu_no].header['NAXIS1']
@@ -33,15 +37,29 @@ class MapsList(object):
             ras.append(ra)
             decs.append(dec)
             files.append(d)
+            wcs.append(w)
+            sizes.append((xsize,ysize))
             hdu.close()
         self.files=files
         self.ras=np.array(ras)
         self.decs=np.array(decs)
+        self.sizes=sizes
+        self.wcs=wcs
     def find(self,ra,dec):
-        dist=(np.cos(self.decs*np.pi/180.0)*(self.ras-ra))**2.0 + (self.decs-dec)**2.0
-        i=np.argmin(dist)
-        return self.files[i]
-
+        dist=separation(ra,dec,self.ras,self.decs)
+        ranks=sorted(range(len(dist)),key=lambda i:dist[i])
+        for r in ranks:
+            w=self.wcs[r]
+            xsize,ysize=self.sizes[r]
+            try:
+                x,y=w.wcs_world2pix(ra,dec,0)
+            except TypeError:
+                x,y,_,_=w.wcs_world2pix(ra,dec,0,0,0)
+            if x>=0 and y>=0 and x<xsize and y<ysize:
+                break
+        else:
+            raise RuntimeError('Cannot find suitable map')
+        return self.files[r]
 
 if __name__=='__main__':
     t=Table.read(sys.argv[1])
@@ -55,12 +73,19 @@ if __name__=='__main__':
    
     imagedir=os.environ['IMAGEDIR']
     os.chdir(imagedir)
-
-    lm=MapsList('mosaics/*-mosaic.fits')
-    os.chdir('downloads')
-    psm=MapsList('rings*.fits',hdu_no=1)
-    wm=MapsList('*w1*fits')
-    fm=MapsList('*%2B*.fits')
+    if os.path.isfile('mapslist.pickle'):
+        with open('mapslist.pickle') as f:
+            lm,psm,wm,fm=pickle.load(f)
+    else:
+        stop
+        lm=MapsList('mosaics/*-mosaic.fits')
+        os.chdir('downloads')
+        psm=MapsList('rings*.fits',hdu_no=1)
+        wm=MapsList('*w1*fits')
+        fm=MapsList('*%2B*.fits')
+        os.chdir(imagedir)
+        with open('mapslist.pickle','w') as f:
+            pickle.dump([lm,psm,wm,fm],f)
 
     for r in t[startpoint:]:
         lofarname=lm.find(r['RA'],r['DEC'])
