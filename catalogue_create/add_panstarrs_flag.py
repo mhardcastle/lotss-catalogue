2e4d60c91241d5fb3e595a83624efeac30aac1a8
Martin Hardcastle <mjh@extragalactic.info>
2018-06-01 16:34:20 +0100

    Add Panstarrs flagging code

---
+++ catalogue_create/add_panstarrs_flag.py
@@ -0,0 +1,10 @@
+from astropy.table import Table
+
+t=Table.read('LOFAR_HBA_T1_DR1_merge_ID_optical_v1.2.fits')
+psf=t['RA']<184.445
+psf&=t['DEC']>55.000
+psf&=t['DEC']<55.2245
+
+t['PanSTARRS_missing']=psf
+
+t.write('LOFAR_HBA_T1_DR1_merge_ID_optical_f_v1.2.fits')
