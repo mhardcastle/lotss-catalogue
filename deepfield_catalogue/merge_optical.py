01af5b7a8ebfae9ae0e993e591c924a5b3cdc1f8
Martin Hardcastle <mjh@extragalactic.info>
2019-11-18 21:31:19 +0000

    further merge fixes

--- deepfield_catalogue/merge_optical.py
+++ deepfield_catalogue/merge_optical.py
@@ -21,7 +21,7 @@ if field=='en1':
 elif field=='bootes':
     mask='/beegfs/lofar/deepfields/Bootes_merged_optical/radio_optical_overlap_masks/image_full_ampphase_di_m.NS_shift.blanked.scaled.rms_spmask.fits'
     optcat='/beegfs/lofar/deepfields/Bootes_merged_optical/add_uncat/Bootes_MASTER_opt_spitzer_merged_adduncat_lite.fits'
-    src='/beegfs/lofar/deepfields/science_ready_catalogs/Bootes_opt_spitzer_merged_vac_opt3as_irac4as_all_hpx_public.fits'
+    src='/beegfs/lofar/deepfields/science_ready_catalogs/Bootes_opt_spitzer_merged_vac_opt3as_irac4as_all_hpx_masses_public.fits'
 elif field=='lockman':
     mask='/beegfs/lofar/deepfields/Lockman_edited_cats/radio_optical_overlap_masks/image_full_ampphase_di_m.NS_shift.blanked.scaled.rms_spmask.fits'
     optcat='/beegfs/lofar/deepfields/Lockman_edited_cats/optical/add_uncat/LH_MASTER_opt_spitzer_merged_cedit_apcorr_adduncat_lite.fits'
@@ -60,23 +60,23 @@ t=Table.read(mergeout2)
 finalname=mergeout2.replace('merged_src','final')
 print 'Remove unnecessary columns'
 
-cols=['RA_2','DEC_2','FLAG_OVERLAP_2','FLAG_CLEAN_2','id', 'ID_OPTICAL_2', 'ID_SPITZER_2','ID_2','EBV_2']
+cols=['RA_2','DEC_2','FLAG_OVERLAP_2','FLAG_CLEAN_2','id', 'ID_OPTICAL_2', 'ID_SPITZER_2','ID_2','EBV_2','FLAG_DEEP_2']
 for fcol in t.colnames:
     if fcol.endswith("_fluxerr"):
         cols.append(fcol)
         cols.append(fcol[:-3])
 
 for c in cols:
-    print c,
-    sys.stdout.flush()
     if c in t.colnames:
+        print c,
+        sys.stdout.flush()
         t.remove_columns(c)
 
 print
 print 'Rename columns'
 #t['Separation_1'].name='Separation'
 
-for column in ['ID','RA','DEC','FLAG_OVERLAP','flag_clean', 'ID_OPTICAL', 'ID_SPITZER','EBV']:
+for column in ['ID','RA','DEC','FLAG_OVERLAP','flag_clean', 'ID_OPTICAL', 'ID_SPITZER','EBV','FLAG_DEEP']:
 
     oldcol=column+'_1'
     if oldcol in t.colnames:
