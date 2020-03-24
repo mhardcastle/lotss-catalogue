4db8c587a7239a12de548b7616e14dc1f577ae54
Martin Hardcastle <mjh@extragalactic.info>
2020-03-24 10:05:03 +0000

    update for lockman

--- deepfield_catalogue/merge_optical.py
+++ deepfield_catalogue/merge_optical.py
@@ -25,6 +25,7 @@ elif field=='bootes':
     optcat='/beegfs/lofar/deepfields/Bootes_merged_optical/add_uncat/Bootes_MASTER_opt_spitzer_merged_adduncat_lite.fits'
     src='/beegfs/lofar/deepfields/science_ready_catalogs/Bootes_opt_spitzer_merged_vac_opt3as_irac4as_all_hpx_masses_public.fits'
     xid='/beegfs/lofar/deepfields/Bootes_FIR/XID+_lofar_Bootes_v0.5_20200209.fits'
+    src_ze=None
 elif field=='lockman':
     mask='/beegfs/lofar/deepfields/Lockman_edited_cats/radio_optical_overlap_masks/image_full_ampphase_di_m.NS_shift.blanked.scaled.rms_spmask.fits'
     optcat='/beegfs/lofar/deepfields/Lockman_edited_cats/optical/add_uncat/LH_MASTER_opt_spitzer_merged_cedit_apcorr_adduncat_lite.fits'
@@ -59,25 +60,28 @@ mergeout2=mergeout.replace('merged','merged_src')
 #    t['NUMBER'].mask=np.isnan(t['ID'])
 #    t.write(mergeout,overwrite=True)
 
-# Join the src and src_ze columns
-src_t = Table.read(src)
-src_ze_t = Table.read(src_ze)
+if src_ze is not None:
+    # Join the src and src_ze columns
+    src_t = Table.read(src)
+    src_ze_t = Table.read(src_ze)
 
-new_z_cols = ["Z_SPEC", "Z_SOURCE", "Z_QUAL"]
-for col in new_z_cols:
-    src_t[col] = src_ze_t[col]
-del src_ze_t
-# Write this to a tmp file
-src_v2_fname = field + "_src_v2_tmp.fits"
-src_t.write(src_v2_fname, overwrite=True)
-
+    new_z_cols = ["Z_SPEC", "Z_SOURCE", "Z_QUAL"]
+    for col in new_z_cols:
+        src_t[col] = src_ze_t[col]
+    del src_ze_t
+    # Write this to a tmp file
+    src_v2_fname = field + "_src_v2_tmp.fits"
+    src_t.write(src_v2_fname, overwrite=True)
+else:
+    #just use the SRC
+    src_v2_fname=src
 os.system('/soft/topcat/stilts tmatch2  join=all1 values1=ID values2=ID matcher=exact in1=%s in2=%s out=%s' % (mergeout,src_v2_fname,mergeout2))
 
 # Delete the tmp src file
 if os.path.exists(src_v2_fname):
     os.system("rm " + src_v2_fname)
 
-t=Table.read(mergeout2, character_as_bytes=False)
+t=Table.read(mergeout2)
 finalname=mergeout2.replace('merged_src','final')
 print 'Remove unnecessary columns'
 
